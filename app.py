cd9c04e90009e961e101ce59624c0a19a048a453
novel2024 <158158875+novel2024@users.noreply.github.com>
2024-04-08 15:34:41 +0800

    port issue

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+import os
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/')
-def home():
-    return '又要到饭了兄弟们，哈哈哈哈哈'
-
+def index():
+    return "Welcome, this is a Flask app deployed on Zeabur"
+ 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
